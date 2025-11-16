06c67d9e78085bf2fb2558bf314ee0b8d923004c
Lance <lancecrucis123@gmail.com>
2025-11-16 13:48:50 +0800

    Revert "changed the title to DISASTER RELIEF DONATION SYSTEM"
    This reverts commit e68241858bc8487bcfc895b93c849fc5b436eac4.

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -57,11 +57,11 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(145, 133);
+            this.label2.Location = new System.Drawing.Point(182, 137);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(254, 20);
+            this.label2.Size = new System.Drawing.Size(182, 20);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Disaster Relief Donation System";
+            this.label2.Text = "Disaster Relief System";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
